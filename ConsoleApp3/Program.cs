652df066033dd77df8f789f11069250e907874dc
VINCE.AUSTRIA <VINCE.AUSTRIA@ITLAB2-04>
2024-08-27 13:54:24 +0800

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+string employeeName = ("Elsa Versailles");
+string address = ("Manila PH");
+string education = ("College");
+string contactNumber = ("09128673214");
+string emailAdd = ("elsa@coldmail.com");
+int expectedSalary = (30000);
+int experience = (5);
+int age = (24);
+
+Console.WriteLine("Employee Console \n");
+
+Console.WriteLine("Name: " + employeeName);
+Console.WriteLine("Address: " + address);
+Console.WriteLine("Education: " + education);
+Console.WriteLine("Phone #: " + contactNumber);
+Console.WriteLine("Email: " + emailAdd);
+Console.WriteLine("Salary: " + expectedSalary);
+Console.WriteLine("Work Experience: " + experience);
+Console.WriteLine("Age: " + age);
+
+int x = 1;
+int y = 1;
+Console.WriteLine(x + y);
+*/
+
+string input = Console.ReadLine();
+int exp = Convert.ToInt32(input);
+Console.WriteLine("You entered: " + (exp + 1));
+
